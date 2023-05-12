512346feb87179119b131d5a3c6c414848bc02f8
Dmitry Sky <dmitry.sky@icloud.com>
2023-05-11 18:15:31 -0600

    Navabr layout, style, and dark mode toggle

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,7 +1,18 @@
-function Layout() {
-  return (
-    <div>Layout</div>
-  )
-}
+import { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "../../components/Navbar";
 
-export default Layout
+const Layout = () => {
+  return (
+    <Box width="100%" height="100%">
+      <Box>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
